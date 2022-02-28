63652278dd0f39f435ec607a7830c466d1f47aaf
Andrés Achury <aachury@serempre.com>
2022-02-28 02:22:12 -0500

    sytles to create a contract

---
+++ src/styles/form.styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components/macro";
+import tw from "twin.macro";
+
+export const StyledForm = styled.form.attrs(() => ({
+  className: "StyledForm",
+}))`
+  ${tw` flex flex-col justify-center items-center mt-10 space-y-4`}
+`;
+
+export const StyledTwoColumns = styled.section.attrs(() => ({
+  className: "StyledTwoColumns",
+}))`
+  ${tw` w-full mx-auto flex flex-col lg:flex-row justify-evenly space-x-4`}
+
+  .MuiInputLabel-root {
+    ${(props) => props.disabledTransition && tw`block`}
+  }
+
+  .MuiOutlinedInput-root {
+    ${tw` w-full `}
+  }
+`;
--- src/views/screens/Admin/Contract/CreateContract.jsx
+++ src/views/screens/Admin/Contract/CreateContract.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useMemo } from "react";
+import _ from "lodash";
+import { useForm } from "react-hook-form";
+import { City } from "country-state-city";
 
 //UI Componentes
 import TextField from "@mui/material/TextField";
@@ -7,12 +11,33 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import Button from "@mui/material/Button";
 
 //Icons/Assets
 import { BiArrowBack } from "react-icons/bi";
 
+//Styles
+import { StyledForm, StyledTwoColumns } from "../../../../styles/form.styles";
+
 const CreateContract = (props) => {
   const { handleChangeScreen } = props;
+
+  const { register } = useForm();
+
+  const [citiesOfColombian, setCitiesOfColombian] = useState();
+
+  useEffect(() => {
+    setCitiesOfColombian(City.getCitiesOfCountry("CO"));
+  }, []);
+
+  const optionsCitiesOfColombia = useMemo(
+    () =>
+      _.map(citiesOfColombian, (city) => ({
+        label: city.name,
+        value: city.name,
+      })),
+    [citiesOfColombian]
+  );
 
   return (
     <>
@@ -25,7 +50,7 @@ const CreateContract = (props) => {
 
       <h2 className="text-center font-bold text-2xl"> Crear contrato </h2>
 
-      <form className="flex flex-col justify-center mt-10 space-y-4">
+      <StyledForm>
         <FormControl variant="standard" fullWidth>
           <InputLabel id="demo-simple-select-standard-label">
             {" "}
@@ -38,9 +63,6 @@ const CreateContract = (props) => {
             //   onChange={handleChange}
             label="Tipo de contrato"
           >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
             <MenuItem value="tf">Termino fijo</MenuItem>
             <MenuItem value="ps">Prestacion de servicios</MenuItem>
             <MenuItem value="ps">Termino fijo inferior a un año</MenuItem>
@@ -49,177 +71,217 @@ const CreateContract = (props) => {
           </Select>
         </FormControl>
 
-        <TextField
-          id="outlined-basic"
-          label="Lugar de Expedicion"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Fecha de nacimiento"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Caja de compensasion"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Fondo de pensiones"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
+        <StyledTwoColumns disabledTransition>
+          <FormControl variant="standard" fullWidth>
+            <InputLabel id="demo-simple-select-standard-label">
+              {" "}
+              Selecciona el lugar de expedicion
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              //   value={age}
+              //   onChange={handleChange}
+              label="Lugar de expeidicion"
+              className="w-full"
+            >
+              {_.map(optionsCitiesOfColombia, (city) => {
+                return <MenuItem value={city.value}>{city.label}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
+
+          <TextField
+            id="outlined-basic"
+            label="Fecha de expedicion"
+            variant="standard"
+            className="block w-full"
+            type="date"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+
+            // {...register("email")}
+          />
+        </StyledTwoColumns>
+
+        <StyledTwoColumns>
+          <TextField
+            id="outlined-basic"
+            label="Caja de compensasion"
+            variant="outlined"
+            className="block w-full"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Fondo de pensiones"
+            variant="outlined"
+            className="block w-full"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+        </StyledTwoColumns>
         <TextField
           id="outlined-basic"
           label="EPS"
           variant="outlined"
-          className="block"
+          className="block w-full"
           // error={errors["email"]?.message}
           // helperText={errors["email"]?.message}
           required
           // {...register("email")}
         />
-        <TextField
-          id="outlined-basic"
-          label="Fecha de ingreso"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Fecha de retiro"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Salario"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Auxilio por trayecto"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Auxilio por transporte"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Cargo"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Titilo universitario"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Universidad"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Telefono"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Dirección"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
-        <TextField
-          id="outlined-basic"
-          label="E-Mail"
-          variant="outlined"
-          className="block"
-          // error={errors["email"]?.message}
-          // helperText={errors["email"]?.message}
-          required
-          // {...register("email")}
-        />
+        <StyledTwoColumns disabledTransition>
+          <TextField
+            id="outlined-basic"
+            label="Fecha de ingreso"
+            variant="standard"
+            className="block w-full"
+            type="date"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+
+            // {...register("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Fecha de retiro"
+            variant="standard"
+            className="block w-full"
+            type="date"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+        </StyledTwoColumns>
+        <StyledTwoColumns>
+          <TextField
+            id="outlined-basic"
+            label="Salario"
+            variant="outlined"
+            className="block w-full"
+            type="number"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Auxilio por trayecto"
+            variant="outlined"
+            className="block w-full"
+            type="number"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Auxilio por transporte"
+            variant="outlined"
+            className="block w-full"
+            type="number"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+        </StyledTwoColumns>
+        <StyledTwoColumns>
+          <TextField
+            id="outlined-basic"
+            label="Cargo"
+            variant="outlined"
+            className="block w-full"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Titilo universitario"
+            variant="outlined"
+            className="block w-full"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+        </StyledTwoColumns>
+        <StyledTwoColumns>
+          <TextField
+            id="outlined-basic"
+            label="Universidad"
+            variant="outlined"
+            className="block w-full"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Telefono"
+            variant="outlined"
+            className="block w-full"
+            type="number"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+        </StyledTwoColumns>
+        <StyledTwoColumns>
+          <TextField
+            id="outlined-basic"
+            label="Dirección"
+            variant="outlined"
+            className="block w-full"
+            type="email"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+          <TextField
+            id="outlined-basic"
+            label="E-Mail"
+            variant="outlined"
+            className="block w-full"
+            // error={errors["email"]?.message}
+            // helperText={errors["email"]?.message}
+            required
+            // {...register("email")}
+          />
+        </StyledTwoColumns>
         <TextField
           id="outlined-basic"
           label="Observaciónes"
           variant="outlined"
-          className="block"
+          className="block w-full"
           // error={errors["email"]?.message}
           // helperText={errors["email"]?.message}
           required
+          multiline
+          rows={4}
           // {...register("email")}
         />
-      </form>
+        <Button variant="contained" className="w-40">
+          {" "}
+          Crear
+        </Button>
+      </StyledForm>
     </>
   );
 };
