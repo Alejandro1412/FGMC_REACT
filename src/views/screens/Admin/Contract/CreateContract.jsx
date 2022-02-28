23b8ec3f186993c61ee4805dc8fe715b697e88b5
AlejandroPolancoA <alejopola14@hotmail.com>
2022-02-27 23:35:09 -0500

    wip: logical for create new contract

--- src/views/screens/Admin/Contract/CreateContract.jsx
+++ src/views/screens/Admin/Contract/CreateContract.jsx
@@ -43,12 +43,15 @@ const CreateContract = (props) => {
             </MenuItem>
             <MenuItem value="tf">Termino fijo</MenuItem>
             <MenuItem value="ps">Prestacion de servicios</MenuItem>
+            <MenuItem value="ps">Termino fijo inferior a un año</MenuItem>
+            <MenuItem value="ps">Agente educativo</MenuItem>
+            <MenuItem value="ps">Contrato docentes</MenuItem>
           </Select>
         </FormControl>
 
         <TextField
           id="outlined-basic"
-          label="Correo"
+          label="Lugar de Expedicion"
           variant="outlined"
           className="block"
           // error={errors["email"]?.message}
@@ -58,7 +61,7 @@ const CreateContract = (props) => {
         />
         <TextField
           id="outlined-basic"
-          label="Correo"
+          label="Fecha de nacimiento"
           variant="outlined"
           className="block"
           // error={errors["email"]?.message}
@@ -68,7 +71,147 @@ const CreateContract = (props) => {
         />
         <TextField
           id="outlined-basic"
-          label="Correo"
+          label="Caja de compensasion"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Fondo de pensiones"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="EPS"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Fecha de ingreso"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Fecha de retiro"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Salario"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Auxilio por trayecto"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Auxilio por transporte"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Cargo"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Titilo universitario"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Universidad"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Telefono"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Dirección"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="E-Mail"
+          variant="outlined"
+          className="block"
+          // error={errors["email"]?.message}
+          // helperText={errors["email"]?.message}
+          required
+          // {...register("email")}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Observaciónes"
           variant="outlined"
           className="block"
           // error={errors["email"]?.message}
