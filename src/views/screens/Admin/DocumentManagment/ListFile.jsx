c9f2a61914191840d9c91a67b023588f96fa64d8
Andrés Achury <achuryjaramillo@gmail.com>
2022-03-01 01:03:54 -0500

    styles to show all data about documents

--- src/views/screens/Admin/DocumentManagment/ListFile.jsx
+++ src/views/screens/Admin/DocumentManagment/ListFile.jsx
@@ -1,12 +1,25 @@
 //Packages
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
+import _ from "lodash";
 
 //Componentes
 import Button from "@mui/material/Button";
 
+//Assets
+import { BiDownload } from "react-icons/bi";
+
 const ListFile = (props) => {
   const { handleChangeScreen } = props;
+
+  const dataFake = useRef([
+    { name: "Foto grado", url: "https://google.com" },
+    { name: "Pdf universidades", url: "https://google.com" },
+    { name: "Programacion", url: "https://google.com" },
+    { name: "Nomina", url: "https://google.com" },
+    { name: "Gobierno del cauca", url: "https://google.com" },
+    { name: "Seguridad y salud en el trabajo", url: "https://google.com" },
+  ]);
 
   return (
     <>
@@ -18,6 +31,26 @@ const ListFile = (props) => {
       >
         Crear documento
       </Button>
+
+      <h2 className="text-center font-bold text-2xl">
+        {" "}
+        Documentos disponibles{" "}
+      </h2>
+
+      <section className="flex flex-col justify-center items-center space-y-4 mt-10 w-full">
+        {dataFake.current.length > 0 &&
+          _.map(dataFake.current, (file, index) => {
+            return (
+              <div
+                className="shadow-2xl rounded-md w-1/2 bg-gray-400 uppercase m-auto flex justify-between px-4"
+                key={`fileItem-${index}`}
+              >
+                <p className="px-4 underline font-semibold"> {file.name} </p>
+                <BiDownload className="w-8 h-8 cursor-pointer" />
+              </div>
+            );
+          })}
+      </section>
     </>
   );
 };
